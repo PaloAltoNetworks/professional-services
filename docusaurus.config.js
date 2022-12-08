f3710c3ed4c13c86f1fb44a3aea49bde5f74d07d
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2022-12-08 09:51:57 +0000

    Tag fetching refactor Add indexes to React components Add IdealImage Component

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,16 @@ const config = {
 
   plugins: [
     "docusaurus-plugin-sass",
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
   ],
 
   stylesheets: [
